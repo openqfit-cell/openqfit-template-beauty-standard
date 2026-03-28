85befae71b901955f1a01117cdd5bfb2b9658303
Jungsu <freeyeo11@gmail.com>
2026-03-28 21:45:31 +0900

    푸터 내용 수정

--- src/app/data/footerData.ts
+++ src/app/data/footerData.ts
@@ -7,12 +7,6 @@ export const footerData: FooterData = {
     name: brandName,
     tagline: '프리미엄 뷰티 케어 전문 당신의 아름다움을 완성합니다',
   },
-  labels: {
-    navigation: '메뉴',
-    services: '서비스',
-    contact: '연락처',
-    businessHours: '영업시간',
-  },
   socialLinks: [
     {
       platform: 'Instagram',
@@ -30,22 +24,5 @@ export const footerData: FooterData = {
       icon: Youtube,
     },
   ],
-  navigation: mainNavigation,
-  services: servicesList,
-  contact: {
-    address: contactInfo.address,
-    phone: {
-      number: contactInfo.phone.number,
-      display: contactInfo.phone.display,
-    },
-    email: {
-      address: contactInfo.email.address,
-      display: contactInfo.email.display,
-    },
-    businessHours: [
-      { label: '평일', hours: '10:00 - 20:00' },
-      { label: '토요일', hours: '10:00 - 19:00' },
-    ],
-  },
   copyright: '© 2026 La Belle. All rights reserved.',
 };
