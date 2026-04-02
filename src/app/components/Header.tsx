3eb646ba447dee25186652d7a049d1f1ddf94530
Jungsu <freeyeo11@gmail.com>
2026-04-02 18:41:27 +0900

    헤더 높이 수정

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -35,7 +35,7 @@ export function Header() {
         }`}
       >
         <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-14 lg:h-20">
+          <div className="flex items-center justify-between h-14 lg:h-16">
             {/* Logo */}
             <button
               onClick={() => {
